78f17caa0a1e39d354aa115f1608311c225e2b93
Nichita <nichita.afanasiev@gmail.com>
2021-11-07 18:23:24 +0300

    Initial commit

---
+++ TicTacTow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TicTacTow
+//
+//  Created by Nichita Afanasiev on 07.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TicTacTow/TicTacTowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacTowApp.swift
+//  TicTacTow
+//
+//  Created by Nichita Afanasiev on 07.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TicTacTowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
